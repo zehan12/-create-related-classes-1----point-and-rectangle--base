7585c41438fc33db143bc65b7cd1c01dc5c3cfa4
Zehan Khan <zkhan@hootboard.com>
2023-07-20 01:37:51 +0530

    import class into class

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,4 +1,25 @@
+import java.awt.*;
 public class Rectangle {
     // write the code of rectangle class here
     Point topLeft;
+    int height;
+    int width;
+
+    public Rectangle(Point t, int h, int w) {
+        this.topLeft = t;
+        this.height = h;
+        this.width = w;
+    }
+
+    public boolean display() {
+        System.out.println(this.width);
+//        return this.topLeft;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Point t = new Point(444,2);
+        Rectangle rect = new Rectangle(t,3,3);
+        System.out.println(rect.display());
+    }
 }
